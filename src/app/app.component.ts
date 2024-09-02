f3269392d1a822731b7a341687ac546df4c8da7d
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-09-02 18:15:02 +0600

    upgrade version 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { environment } from '../environments/environments';
 import { DataService } from './services/data.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   private dataService = inject(DataService);
